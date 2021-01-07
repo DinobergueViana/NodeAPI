8eb1902068ca39f6f1b2e84f91ef643203a03c4c
DinobergueViana <dinobergueviana@gmail.com>
2021-01-07 10:37:25 -0300

    creates the basics routes for CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,9 @@ const UserController = require("../app/controllers/api/UserController");
 const routes = express.Router();
 
 routes.get("/users", UserController.index);
-routes.get("user/:id", UserController.searchById);
+routes.get("/users/:id", UserController.searchById);
+routes.post("/users/create", UserController.create);
+routes.put("/users/update/:id", UserController.update);
+routes.delete("/users/delete/:id", UserController.delete);
 
 module.exports = routes;
