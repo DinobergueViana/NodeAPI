a5d9eb7ae771f3b47e803f61af484f0bfd08b686
DinobergueViana <dinobergueviana@gmail.com>
2021-01-07 10:39:07 -0300

    creates the basic methods in UserController

--- src/app/controllers/api/UserController.js
+++ src/app/controllers/api/UserController.js
@@ -10,7 +10,7 @@ module.exports = {
             return res.status(400).json(error)
         })
     },
-    async searcById(req, res){
+    async searchById(req, res){
         let id = req.params.id
 
         await User.findByPk(id)
@@ -18,7 +18,56 @@ module.exports = {
             return res.status(200).json(user);
         })
         .catch(error => {
-            return res.status(400).json(error)
+            return res.status(400).json("Error processing request" + error)
         })
+    },
+    async create(req, res){
+        let {
+            email,
+            password_hash
+        } = req.body;
+
+        await User.create({
+            email: email,
+            password_hash: password_hash
+        })
+        .then(user => {
+            return res.status(200).json(user);
+        })
+        .catch(error => {
+            return res.status(400).json("Error processing request" + error)
+        })
+    },
+    async update(req, res){
+        let id = req.params.id;
+
+        const {email, password_hash} = req.body;
+
+        await User.update({
+            email: email,
+            password_hash: password_hash
+        },
+        { where: { id:id }})
+        .then(user => {
+            return res.status(200).json("User updated successfully");
+        })
+        .catch(error => {
+            return res.status(400).json("Error processing request" + error);
+        })
+    },
+    async delete(req, res){
+        let id = req.params.id
+
+        await User.destroy(
+            { 
+                where: {id: id } 
+            })
+            .then(user => {
+                return res.status(200).json("User deleted successfully")
+            })
+            .catch(error => {
+                return res.staus(400).json("Error processing request" + error)
+            })
+
     }
 }
