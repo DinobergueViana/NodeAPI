d152f28844e105654f6b63ed9e2b0e28b78a4b00
DinobergueViana <dinobergueviana@gmail.com>
2021-01-07 16:11:24 -0300

    creates authenticate and check routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const UserController = require("../app/controllers/api/UserController");
+const authMiddleware = require("../app/middlewares/auth");
 
 const routes = express.Router();
 
+routes.post("/users/create", UserController.create);
+routes.post("/users/authenticate", UserController.authenticate);
+
+routes.use(authMiddleware)
+
 routes.get("/users", UserController.index);
 routes.get("/users/:id", UserController.searchById);
-routes.post("/users/create", UserController.create);
+
 routes.put("/users/update/:id", UserController.update);
 routes.delete("/users/delete/:id", UserController.delete);
+
+routes.post("/users/check", UserController.check);
 
 module.exports = routes;
